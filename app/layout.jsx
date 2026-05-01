b757162f2da958707ec8a4fe5d3490b52ab3262d
apriln82-png <apriln82@gmail.com>
2026-04-30 21:08:18 -0500

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,25 @@
+export const metadata = {
+
+  title: "BZ Customs",
+
+  description: "Welding and Custom Metal Fabrication in Oklahoma City",
+
+};
+
+export default function RootLayout({ children }) {
+
+  return (
+
+    <html lang="en">
+
+      <body>
+
+        {children}
+
+      </body>
+
+    </html>
+
+  );
+
+}
